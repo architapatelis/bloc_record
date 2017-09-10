763a4504537f3ae4ce5cf2b0c1c4d20d01b1e385
Archita Patel <architapatelis@gmail.com>
2017-09-09 21:16:38 -0700

    checkpoint 3 assignment completed

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -9,6 +9,13 @@ module Selection
     if ids.length == 1
       find_one(ids.first)
     else
+      ids.each do |id|
+        if id.is_a?(Integer) && id > 0
+          next
+        else
+          puts "Ids must be numbers greater than 0. Try again"
+        end
+      end
       rows = connection.execute <<-SQL
         SELECT #{columns.join ","} FROM #{table}
         WHERE id IN (#{ids.join(",")});
@@ -20,13 +27,17 @@ module Selection
 
   # used for example: character = Character.find(7)
   def find_one(id)
-    row = connection.get_first_row <<-SQL
-      SELECT #{columns.join ","} FROM #{table}
-      WHERE id = #{id};
-    SQL
+    if id.is_a?(Integer) && id > 0
+      row = connection.get_first_row <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        WHERE id = #{id};
+      SQL
 
-    # converts a row into an object
-    init_object_from_row(row)
+      # converts a row into an object
+      init_object_from_row(row)
+    else
+      puts "Id must be a number greater than 0, Try again"
+    end
   end
 
   # myAddressBook = AddressBook.find_by("name", "My Address Book")
@@ -95,7 +106,25 @@ module Selection
 
     rows_to_array(rows)
   end
-  
+
+  # Contact.find_each(start: 2000, batch_size: 2000)
+  def find_each(options = {})
+    rows = retrieve_records(options) #private method
+
+    rows.each do |row|
+      init_object_from_row(row)
+    end
+  end
+
+  def find_in_batches(options={})
+    rows = retrieve_records(options) #private method
+
+    row_array = rows_to_array(rows)
+    row_array
+  end
+
+# ******************************************************************************
+
   private
   # converts a row into an object
   def init_object_from_row(row)
@@ -108,5 +137,51 @@ module Selection
   # Given an array of rows, this method maps the rows to an array of corresponding objects.
   def rows_to_array(rows)
     rows.map { |row| new(Hash[columns.zip(row)]) }
+  end
+
+
+  def method_missing(m, *args) # m refers to name_of_method
+    # e.g. find_by_name
+    if m.match(/find_by_/)
+      dynamic_name = m.to_s.split('find_by_')[1] # dynamic_name = name
+      if columns.include?(dynamic_name) #if table includes a column called 'name'
+        find_by(dynamic_name, *args) # find_by(name, *args)
+      else
+        raise "#{m} is not a valid method."
+      end
+    end
+  end
+
+  def retrieve_records(options)
+    start = options.has_key?(:start) ? options[:start] : nil # start at 2000
+    batch_size = options.has_key?(:batch_size) ? options[:batch_size] : nil #batch_size is 2000, (from 2001 to at most 4000)
+    if start != nil && batch_size != nil
+      # LIMIT 2000 OFFSET 2000
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        LIMIT #{batch_size} OFFSET #{start};
+      SQL
+    # Contact.find_each(batch_size: 2000)
+    # start at first row, and go at most up to batch_size
+    elsif start == nil && batch_size != nil
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        LIMIT #{batch_size};
+      SQL
+    # Contact.find_each(start: 2000)
+    # from 2001 to end of table
+    # cant not set OFFSET without LIMIT, therefore set LIMIT to -1
+    elsif start != nil && batch_size == nil
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table}
+        LIMIT -1 OFFSET #{start};
+      SQL
+    # no start or batch_size given
+    # list all columns in a table
+    else
+      rows = connection.execute <<-SQL
+        SELECT #{columns.join ","} FROM #{table};
+      SQL
+    end
   end
 end
