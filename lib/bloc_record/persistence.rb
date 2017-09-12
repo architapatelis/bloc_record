205772b3cedd3bfec0c46899f64f06dc81a80072
Archita Patel <architapatelis@gmail.com>
2017-09-12 08:46:18 -0700

    completed checkpoint6 - destroy

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -4,6 +4,11 @@ require 'bloc_record/schema'
 
 # self.included is called whenever this module is included.
 # When this happens, extend adds the ClassMethods methods to Persistence.
+# methods that are not witin ClassMethods, are instance methods:
+  # e = Entry.first
+  # e is an instance of Entry Class
+  # therefore, we can use e.destroy or e.update_attribute.
+  # remember e.destory is like saying, self.class.destroy which is: Entry.destroy
 module Persistence
   def self.included(base)
     base.extend(ClassMethods)
@@ -48,6 +53,12 @@ module Persistence
   # e.update_attributes(name: "Ben", phone_numer: 949-858-7878)
   def update_attributes(updates)
     self.class.update(self.id, updates) # call private update method
+  end
+
+  # e = Entry.first
+  # e.destroy
+  def destroy
+    self.class.destroy(self.id)
   end
 
   # e = Entry.all[3]
@@ -121,9 +132,42 @@ module Persistence
           UPDATE #{table}
           SET #{updates_array * ","} #{where_clause}
         SQL
-
         true
       end
+    end
+
+    # e.g. Entry.destroy(15)
+    # or e.g. Entry.destroy(1,2,3)
+    # remember splat operator(*), makes an array out of multiple arguments.
+    def destroy(*id)
+      if id.length > 1
+        where_clause = "WHERE id IN (#{id.join(",")});"
+      else
+        where_clause = "WHERE id = #{id.first};"
+      end
+      connection.execute <<-SQL
+        DELETE FROM #{table} #{where_clause}
+      SQL
+      true
+    end
+
+    # e.g. Entry.destroy_all or Entry.destroy_all(name: "Jane Doe")
+    # DESTROY will apply to all records if the command isn't scoped using WHERE.
+    def destroy_all(conditions_hash=nil)
+      if conditions_hash && !conditions_hash.empty? # if conditions are given
+        conditions_hash = BlocRecord::Utility.convert_keys(conditions_hash)
+        conditions = conditions_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ") #"name = Jane"
+
+        connection.execute <<-SQL
+          DELETE FROM #{table}
+          WHERE #{conditions};
+        SQL
+      else
+        connection.execute <<-SQL
+          DELETE FROM #{table}
+        SQL
+      end
+      true
     end
   end
 end
